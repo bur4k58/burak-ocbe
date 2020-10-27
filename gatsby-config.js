90648403ac942457c670ed4003e0de2d1bb1aad5
bur4k58 <s116768@ap.be>
2020-10-27 14:16:30 +0100

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,40 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Burak Agency`,
+    description: `Belgium artist Agency`,
+    author: `Burak`,
   },
   plugins: [
+    {
+
+      resolve: "gatsby-source-graphql",
+      
+      options: {
+      
+      typeName: "WPGraphql",
+      
+      fieldName: "wpcontent",
+      
+      url: "http://ocbe-agency.local/.",
+      
+      },
+      
+      },
+      {
+
+        resolve: `gatsby-plugin-google-fonts`,
+        
+        options: {
+        
+        fonts: [`Roboto`, `Oswald`],
+        
+        display: "swap",
+        
+        },
+        
+        }, 
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
